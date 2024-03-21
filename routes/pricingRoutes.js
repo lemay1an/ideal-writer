200b497b1947fabf84db06e579b316b396c4ddd1
lemay1an <mainahmwangi12@gmail.com>
2024-03-21 17:54:22 +0300

    changed environmental variables

--- routes/pricingRoutes.js
+++ routes/pricingRoutes.js
@@ -3,7 +3,7 @@ const pricingCotroller = require("../controllers/pricingController");
 
 const router = express.Router();
 router.post("/createPricing", pricingCotroller.createPricing);
-router.get("/getPricing", pricingCotroller.getPricing);
+router.get("/getPricing/:id", pricingCotroller.getPricing);
 router.get("/getAllPricing", pricingCotroller.getAllPricing);
 router.delete("/deletePricing/:id", pricingCotroller.deletePricing);
 router.patch("/updatePricing/:id", pricingCotroller.updatePricing);
